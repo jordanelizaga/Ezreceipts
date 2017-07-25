57d2b623d36d9398d032f6062fc01af945d69658
Jordan Elizaga <elizaga.jn@gmail.com>
2017-07-25 00:21:31 -0400

    Testing writing to a file and store in Documents folder

--- app/src/main/java/elizaga/j/ezreceipts/MainActivity.java
+++ app/src/main/java/elizaga/j/ezreceipts/MainActivity.java
@@ -2,13 +2,19 @@ package elizaga.j.ezreceipts;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.constraint.Guideline;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
@@ -35,5 +41,36 @@ public class MainActivity extends Activity {
                 }
             }
         });
+
+        // get the path to sdcard
+        // to this path add a new directory path
+        if (canWriteOnExternalStorage()) {
+            File sdcard = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+            //File dir = new File(sdcard);
+            // create this directory if not already created
+            //dir.mkdir();
+            // create the file in which we will write the contents
+            File file = new File(sdcard, "myTextFile.txt");
+            try {
+                FileOutputStream os = new FileOutputStream(file, true); // Boolean sets to append
+                //String data = "This is the content of my file \n This should be a new line \n";
+                String data = "This should be appended \n This should be a new line\n";
+                os.write(data.getBytes());
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static boolean canWriteOnExternalStorage() {
+        // get the state of your external storage
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            // if storage is mounted return true
+            Log.v("myTag", "Yes, can write to external storage.");
+            return true;
+        }
+        return false;
     }
 }
